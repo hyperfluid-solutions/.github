caba0e3a95072bb8735a09cbd1aa6714c68a3abb
Jon Shaffer <jonshaffer@users.noreply.github.com>
2025-02-15 15:15:01 -0500

    chore: Create vars.sh for env vars

---
+++ templates/vars.sh
@@ -0,0 +1,9 @@
+# Sets Environment Variables for template files.
+# 
+# Usage:
+# (source ./vars.sh; target.tpl.md;)
+
+export APP_NAME='replace_me'
+export PRIVACY_EMAIL='replace_me'
+
+export TODAY_FORMAL=$(date '+%d %B %Y')
